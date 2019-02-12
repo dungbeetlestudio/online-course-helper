c80df398757dcd3d7835fc263892e9fe08279571
刘旭 <sl.truman@live.com>
2019-02-12 18:52:42 +0800

    云服务器，机器人

---
+++ teachers/src/pages/interactions/interactions.ts
@@ -0,0 +1,46 @@
+﻿import { Component } from '@angular/core';
+import { ModalController, NavController } from 'ionic-angular';
+import { AddPage } from '../title/add'
+import { Storage } from '@ionic/storage';
+import { HttpClient } from "@angular/common/http"
+import { API } from "../../api-def"
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'interactions.html'
+})
+export class InteractionsPage {
+  items = [
+
+  ]
+
+  constructor(public http: HttpClient, public navCtrl: NavController, public modal: ModalController, public storage: Storage) {
+    storage.get('interactions').then(items => this.items = items == null ? [] : items)
+  }
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item)
+    this.http.get(API.Say, { params: { msg: item } })
+      .subscribe(res => {
+        console.log(res)
+      }, err => {
+        console.error(err)
+      })
+  }
+
+  addClick() {
+    let m = this.modal.create(AddPage)
+    m.present()
+    m.onDidDismiss(name => {
+      if (name != null) {
+        this.items.push(name)
+        this.storage.set('interactions', this.items)
+      }
+    })
+  }
+
+  remove(item) {
+    this.items.splice(this.items.indexOf(item), 1)
+    this.storage.set('interactions', this.items)
+  }
+}
