fb1c72624f7ea79ef9f52c3f56c2997212bd92ad
刘旭 <sl.truman@live.com>
2019-02-13 00:17:53 +0800

    上传到服务器

--- teachers/src/api-def.ts
+++ teachers/src/api-def.ts
@@ -1,7 +1,7 @@
 export const API = {
-    NumberOfRobotsOfStatus: 'http://127.0.0.1/online-course-helper/numberOfRobotsOfStatus',
-    Sign:'http://127.0.0.1/online-course-helper/sign',
-    Enter:'http://127.0.0.1/online-course-helper/enter',
-    Leave:'http://127.0.0.1/online-course-helper/leave',
-    Say:'http://127.0.0.1/online-course-helper/say'
+    NumberOfRobotsOfStatus: 'http://www.dungbeetles.xyz/online-course-helper/numberOfRobotsOfStatus',
+    Sign:'http://www.dungbeetles.xyz/online-course-helper/sign',
+    Enter:'http://www.dungbeetles.xyz/online-course-helper/enter',
+    Leave:'http://www.dungbeetles.xyz/online-course-helper/leave',
+    Say:'http://www.dungbeetles.xyz/online-course-helper/say'
 }
